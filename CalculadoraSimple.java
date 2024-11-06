57f32fb879349d04acf74ba3e0e5e557f7bc7661
xRaiker <raiker159@gmail.com>
2024-11-05 20:47:40 -0500

    Corrección de errores en CalculadoraSimple

--- CalculadoraSimple.java
+++ CalculadoraSimple.java
@@ -1,11 +1,11 @@
 public class CalculadoraSimple {
 
     public int sumar(int a, int b) {
-        return a + b 
+        return a + b;
     }
 
     public int multiplicar(int a, int b) {
-        int resultado = a * b
+        int resultado = a * b;
         return resultado;
     }
 
@@ -15,7 +15,7 @@ public class CalculadoraSimple {
         int resultadoSuma = calc.sumar(10, 5);
         int resultadoMultiplicacion = calc.multiplicar(10, 10);
 
-        System.out.println(Resultado de la suma: " + resultadoSuma);
+        System.out.println("Resultado de la suma: " + resultadoSuma);
         System.out.println("Resultado de la multiplicación: " + resultadoMultiplicacion);
     }
 }
